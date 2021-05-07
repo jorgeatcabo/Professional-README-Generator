5c88c779d2772158dc8005848caf40c24766b253
El Roble <san_lucas2005@yahoo.com>
2021-05-07 12:03:53 -0600

    Use index.js file from Utils folder

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
-
 const inquirer = require('inquirer');
 
-inquirer
-  .prompt([
+const questions = [    
     {
       type: 'input',
       message: 'What is the project title?',
@@ -34,19 +32,68 @@ inquirer
       message: 'What are the test instructions?',
       name: 'projectTest',
     },
-  ])
+    {
+      type: 'input',
+      message: 'Github?',
+      name: 'projectGithub',
+    },
+    {
+      type: 'input',
+      message: 'Email?',
+      name: 'projectEmail',
+    },
+    {
+      type: 'checkbox',
+      message: 'License:',
+      name: 'projectLicense',
+      choices: ['MIT', 'CSS', 'JavaScript', 'MySQL'],
+    }
+];
+
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { 
+  fs.writeFile(fileName, 
+`# ${data.projectTitle}\n
+# Project Description\n
+${data.projectDescription}\n
+# Project Installation\n
+${data.projectInstallation}\n
+# Project Usage\n
+${data.projectUsage}\n
+# Project Contribution\n
+${data.projectContribution}\n
+# Project Test\n
+${data.projectTest}\n
+# Project Gihub\n
+${data.projectGithub}\n
+# Project Email\n
+${data.projectEmail}\n
+# Project License\n
+${data.projectLicense}\n`, (err) =>
+  // TODO: Describe how this ternary operator works
+  err ? console.error(err) : console.log( `File ${fileName} created!`)
+  );
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer
+  .prompt(questions)
   .then((response) =>{
-    fs.truncate(`${process.argv[2]}`, 0, function() {
-      //console.log('File Content Deleted')
-    } );
-    fs.appendFile(`${process.argv[2]}`, `#${response.projectTitle}
-    ${response.projectDescription}\n
-    ${response.projectInstallation}\n
-    ${response.projectUsage}\n
-    ${response.projectContribution}\n
-    ${response.projectTest}\n`, (err) =>
-    // TODO: Describe how this ternary operator works
-    err ? console.error(err) : console.log( `File ${process.argv[2]} created!`)
-    );
+    writeToFile(`${process.argv[2]}`,response)    
   }
   );
+}
+
+
+// Function call to initialize app
+init();
+
+/*
+******** Table of contents *********
+*/
+
+
+
